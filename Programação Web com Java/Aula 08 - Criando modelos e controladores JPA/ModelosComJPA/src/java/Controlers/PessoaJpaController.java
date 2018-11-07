76c9d6cab414b38869fbcb8613fc7d1bbd26c4a7
Vinicius dos Santos <vinistos@gmail.com>
2018-11-07 14:59:27 -0200

    Aula 08

---
+++ Programação Web com Java/Aula 08 - Criando modelos e controladores JPA/ModelosComJPA/src/java/Controlers/PessoaJpaController.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlers;
+
+import Controlers.exceptions.NonexistentEntityException;
+import Models.Pessoa;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author vini_
+ */
+public class PessoaJpaController implements Serializable {
+
+    public PessoaJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Pessoa pessoa) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(pessoa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Pessoa pessoa) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            pessoa = em.merge(pessoa);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = pessoa.getId();
+                if (findPessoa(id) == null) {
+                    throw new NonexistentEntityException("The pessoa with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Pessoa pessoa;
+            try {
+                pessoa = em.getReference(Pessoa.class, id);
+                pessoa.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The pessoa with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(pessoa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Pessoa> findPessoaEntities() {
+        return findPessoaEntities(true, -1, -1);
+    }
+
+    public List<Pessoa> findPessoaEntities(int maxResults, int firstResult) {
+        return findPessoaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Pessoa> findPessoaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Pessoa.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Pessoa findPessoa(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Pessoa.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getPessoaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Pessoa> rt = cq.from(Pessoa.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
